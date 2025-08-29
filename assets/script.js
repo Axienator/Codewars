4575ee99554267d35d28fd9b212443bda8bc1b35
paullorenzopsima@gmail.com <zupsaidoo123@gmail.com>
2025-08-29 16:31:47 +0800

    Problem Solved: Break camelCase

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,22 @@
 function solution(string) {
-  //make the string an array
+  const arr =  string.split("")
+  const capArr = []
+  // const capital array = [Casing]
+  arr.forEach((char) => {
+    if(char === char.toUpperCase()) {
+      capArr.push(' ')
+    }
+    capArr.push(char)
+  });
+
+  return capArr.join('')
+
+  
   //create a counter variable on how many upperCases in the word
   //Make a for of loop
   //if count > 0 make space before the UpperCase
   //convert array to string
 }
+
+console.log(solution("camelCasing"));
+
