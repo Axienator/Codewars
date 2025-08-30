a901f7712b51f57e2119d38648e242c909131cfa
Axienator <205501519+Axienator@users.noreply.github.com>
2025-08-30 19:27:37 +0800

    Add Explanation to all problems

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,27 @@
-// function solution(string) {
-//  const arr =  string.split("")
-//  const capArr = []
-//  arr.forEach((char) => {
-//    if(char === char.toUpperCase()) {
-//      capArr.push(' ')
-//    }
-//    capArr.push(char)
-//  });
-
-//  return capArr.join('')
-//}
+ function solution(string) {
+  const arr =  string.split("") // break the string into characters
+  const capArr = [] // Prepare a new array for the result
+  // Loop through each character
+  arr.forEach((char) => {
+    if(char === char.toUpperCase()) {
+      capArr.push(' ')
+    }
+    capArr.push(char) 
+  });
+  return capArr.join('') // join and convert as a string
+}
 
 //console.log(solution("camelCasing"));
 
 
-function humanReadable (seconds) {
-  const m = Math.floor(seconds / 60).toString().padStart(2, '0')
-  const remainM = Math.floor(m % 60).toString().padStart(2, '0')
-  const remainS = Math.floor(seconds % 60).toString().padStart(2, '0')
-  const h = Math.floor(m / 60).toString().padStart(2, '0')
-  return `${h}:${remainM}:${remainS}`
+function humanReadable (seconds) { 
+  const m = Math.floor(seconds / 60).toString().padStart(2, '0') // Compute minutes
+  const remainM = Math.floor(m % 60).toString().padStart(2, '0') // Compute Remaining minutes
+  const remainS = Math.floor(seconds % 60).toString().padStart(2, '0') // Compute Remaining seconds
+  const h = Math.floor(m / 60).toString().padStart(2, '0') // Compute hour
+  return `${h}:${remainM}:${remainS}` // return them with a proper Time format
+
+  // The thought process goes like this: Convert seconds -> minutes and hours -> Use % to handle leftovers -> Pad each unit to always be two digits -> join them altogether into a time string
 }
 
 
